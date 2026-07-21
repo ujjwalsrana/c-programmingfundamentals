726c933bd7722f1603a3d60a6a61eb3eb391cb15
Ujjwal Rana <179561155+ujjwalsrana@users.noreply.github.com>
2026-07-21 11:23:21 +0545

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std; //* in order to not use standard std all the time *//
+int main()
+{
+	cout<<"Hello World"; //* cout is for print out purpose *//
+	return 0;  //* here 0 means your program has run successfully run. Also if not successfeul it will show error *//
+}
