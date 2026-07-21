9502c27447e206e307bb1bb4e857f3d6010b14f6
Ujjwal Rana <179561155+ujjwalsrana@users.noreply.github.com>
2026-07-21 14:00:13 +0545

    Add files via upload

---
+++ Practice_Cin_UserInput.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//Practice 1: calculate fouu number average
+	//float value1, value2, value3, value4, avg;
+	//cout<<"Enter four values: "<<endl;
+	//cin>>value1>>value2>>value3>>value4;
+	//avg = (value1 + value2 + value3 + value4) / 4;
+	//cout<<"The average of "<<value1<<","<<value2<<","<<value3<<" and "<<value4<<" : "<<avg<<endl;
+	
+	//Practice 2: Make String Concatenation
+	string firstname, lastname;
+	cout<<"Enter your first name and laste name: " <<endl;
+	cin>>firstname>>lastname;
+	cout<<"Your Full Name Is: "<<firstname<<" "<<lastname;
+	return 0;
+}
