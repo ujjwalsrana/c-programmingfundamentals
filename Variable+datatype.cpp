42069a30d7128f14b446c1bb73ecfd25c05c0252
Ujjwal Rana <179561155+ujjwalsrana@users.noreply.github.com>
2026-07-21 11:47:05 +0545

    Add files via upload

---
+++ Variable+datatype.cpp
@@ -0,0 +1,37 @@
+#include<ipstream>
+using namespace std;
+int main()
+{
+	/* For multiline comments */
+	// this is my second programming in c++ programming fundamentals
+	/* What is Variables ? Container to Store Data */
+	// How to decleare a variale ? variable can be declare or intialized both. For example check following:
+	int  var1; //  this is the example of variable declare where data type is integers (int) and var1 is variable name
+	
+	//how to initialize varialble where single = means assign whereas if you use --
+	var1 = 5;
+	
+	// how to do variable declearation and initialization
+	int var2 = 80;
+	
+	// Mow you need to understand, what is data types ? data types in simple understanding for any variable what data type is  used. this you need to know.
+	/* there are various data types such as 
+	int ==> whole numbers example 120 or -99
+	float ==> small decimal number for example 64.34
+	double ==> long decimal number for example 164.4555
+	char ==> single character for example c. Note: for char you must always use single quotation for example like 'c'
+	string ==> if you want to give character or text thenn you should use string. For example  "Record Added Successfully". 
+	Note : for string you must always use double quotation "Record Deleted Successfully"
+	bool ==> if you want to use true vale or false the you use bool
+	void ==> void bascially means ematy. if you do not need any return then you use void
+	Now lets use data typles below	*/
+	
+	int var1 - 4;
+	float var2 = 64.54;
+	double var3 = 134.5664;
+	char var4 = 'f' ;
+	string var5 = "Recoard Added";
+	bool var6= true;
+	// not i am not using void its because void is not use for variable declaration
+		retrun 0;
+}
