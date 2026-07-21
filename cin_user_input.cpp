3ab61e44240888876c05458e30e5ce31cd2badfe
Ujjwal Rana <179561155+ujjwalsrana@users.noreply.github.com>
2026-07-21 13:28:30 +0545

    Add files via upload

---
+++ cin_user_input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//int a, b;
+	string a,b;
+	cout<<"Enter the numeric value for a and b "<<endl;
+	
+	//cin ----> user input
+	cin>>a>>b;
+	cout<<"The value for a is: "<<a<<endl;
+	cout<<"The value for b is: "<<b<<endl;
+	cout<<a<<b;
+	return 0;
+}
