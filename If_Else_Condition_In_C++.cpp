a6eee5ee6fa524d23271684df4656a74aa7772f5
Ujjwal Rana <179561155+ujjwalsrana@users.noreply.github.com>
2026-07-22 09:14:11 +0545

    Add files via upload

---
+++ If_Else_Condition_In_C++.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter Your Age: "<<endl;
+	cin>>age;
+	//if else --> conditional statement //
+	//if condition //
+	// {
+	//	//code
+	//	}
+	//	else
+	//	{
+	//	//code
+	//	}
+	
+		if(age>18)
+		{
+		cout<<"Your age is greater then 18";
+		}
+	else
+	{
+		cout<<"Your age is less then 18";
+	}
+	
+	return 0;
+}
