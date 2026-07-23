d294654854eecd7c74d0cf0ce6bd05715c7d8fba
Ujjwal Rana <179561155+ujjwalsrana@users.noreply.github.com>
2026-07-23 17:04:50 +0545

    Add files via upload

---
+++ Do_While_Loop_Practicle_Example1.cpp
@@ -0,0 +1,16 @@
+//DO WHILE LOOP EXAMPLE
+#include<iostream>
+using namespace std;
+int main()
+{
+	char letters = 'A';
+	do{
+		cout<<letters<<" ";
+		letters++;
+	
+	}
+	while(letters<='Z')	;
+	cout<<endl;
+	return 0;
+	
+}
