57ca04f8ab0997f65af1ed57ad1b193bf05fc9bd
Ujjwal Rana <179561155+ujjwalsrana@users.noreply.github.com>
2026-07-23 17:25:21 +0545

    Add files via upload

---
+++ Function_Practical_Example1_in_C++.cpp
@@ -0,0 +1,31 @@
+/*function in c++
+A function in C++ is a block of code that performs a specific task. Instead of writing the same code again and again, 
+you write it once inside a function and call it whenever you need it. Functions make programs easier to read, reuse, 
+and maintain.
+*/
+#include<iostream>
+using namespace std;
+// function syntax
+/*
+data type function-name(parameters)--> formal parameters
+
+{
+return
+}
+*/
+int sum(int a, int b)
+{
+	int add = a+b;
+	return add;
+}
+int main()
+{
+	int num1, num2;
+	cout<<"Enter first Number: ";
+	cin>>num1;
+	cout<<"Entter Second Number: ";
+	cin>>num2;
+	cout<<"The Sum is: " <<sum(num1, num2); // formal parameterers
+	
+	return 0;
+}
